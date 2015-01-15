34b4f25fd3a8d72ccf6a9a515b9bfa2a2498fbf5
RobertoGonzalez <roberto@aerolab.com.ar>
2015-01-15 18:09:31 -0300

    Readme

--- build/blockrain.jquery.js
+++ build/blockrain.jquery.js
@@ -22,26 +22,29 @@
   $.widget('aerolab.blockrain', {
 
     options: {
-      autoplay: false,
-      autoplayRestart: true,
-      showFieldOnStart: true,
-      theme: null,
-      blockWidth: 10,
-      autoBlockWidth: false,
-      autoBlockSize: 24,
-      difficulty: 'normal',
-      speed: 20,
+      autoplay: false, // Let a bot play the game
+      autoplayRestart: true, // Restart the game automatically once a bot loses
+      showFieldOnStart: true, // Show a bunch of random blocks on the start screen (it looks nice)
+      theme: null, // The theme name or a theme object
+      blockWidth: 10, // How many blocks wide the field is (The standard is 10 blocks)
+      autoBlockWidth: false, // The blockWidth is dinamically calculated based on the autoBlockSize. Disabled blockWidth. Useful for responsive backgrounds
+      autoBlockSize: 24, // The max size of a block for autowidth mode
+      difficulty: 'normal', // Difficulty (normal|nice|evil).
+      speed: 20, // The speed of the game. The higher, the faster the pieces go.
 
+      // Copy
       playText: 'Let\'s play some Tetris',
       playButtonText: 'Play',
       gameOverText: 'Game Over',
       restartButtonText: 'Play Again',
       scoreText: 'Score',
 
+      // Basic Callbacks
       onStart: function(){},
       onRestart: function(){},
       onGameOver: function(score){},
 
+      // When a line is made. Returns the number of lines, score assigned and total score
       onLine: function(lines, scoreIncrement, score){}
     },
 
