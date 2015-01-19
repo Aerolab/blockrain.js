903222c09bad148467833341849c47dd498c4d12
RobertoGonzalez <roberto@aerolab.com.ar>
2015-01-19 14:02:45 -0300

    New build script

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
-var gulp = require('gulp')
-, fs = require('fs')
-, uglify = require("gulp-uglify")
-, concat = require("gulp-concat")
-, header = require("gulp-header");
+var gulp = require('gulp'),
+fs = require('fs'),
+uglify = require("gulp-uglify"),
+concat = require("gulp-concat"),
+header = require("gulp-header"),
+zip = require("gulp-zip"),
+runSequence = require('run-sequence');
  
 var getVersion = function () {
     info = require("./package.json");
@@ -12,8 +14,9 @@ var getCopyright = function () {
     return fs.readFileSync('Copyright');
 };
 
-gulp.task('build', function () {
-    gulp.src(['./src/blockrain.jquery.libs.js', './src/blockrain.jquery.src.js', './src/blockrain.jquery.themes.js'])
+gulp.task('js', function () {
+    // Concatenate and Minify JS
+    return gulp.src(['./src/blockrain.jquery.libs.js', './src/blockrain.jquery.src.js', './src/blockrain.jquery.themes.js'])
     .pipe(concat('blockrain.jquery.js'))
     .pipe(header(getCopyright(), {version: getVersion()}))
     .pipe(gulp.dest('./build'))
@@ -21,6 +24,31 @@ gulp.task('build', function () {
     .pipe(concat('blockrain.jquery.min.js'))
     .pipe(header(getCopyright(), {version: getVersion()}))
     .pipe(gulp.dest('./build'));
+});
+
+gulp.task('css', function () {
+    // CSS
+    return gulp.src(['./src/blockrain.css'])
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('readme', function () {
+    // Readme
+    return gulp.src(['./README.md'])
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('dist', function () {
+    // Create a ZIP File
+    return gulp.src('./build/*')
+    .pipe(zip('blockrain.zip'))
+    .pipe(gulp.dest('./dist'));
+});
+
+
+gulp.task('build', function(callback){
+  runSequence('js', 'css', 'readme', 'dist',
+              callback);
 });
 
 gulp.task('default', ['build']);
