8a082af851a20a69c4df4749e8c43450d7ae29bd
Micael Robles <micael.robles@gmail.com>
2017-07-12 17:44:41 -0300

    works on different block widths

--- src/blockrain.jquery.src.js
+++ src/blockrain.jquery.src.js
@@ -9,7 +9,7 @@
       autoplayRestart: true, // Restart the game automatically once a bot loses
       showFieldOnStart: true, // Show a bunch of random blocks on the start screen (it looks nice)
       theme: null, // The theme name or a theme object
-      blockWidth: 10, // How many blocks wide the field is (The standard is 10 blocks)
+      blockWidth: 12, // How many blocks wide the field is (The standard is 10 blocks)
       autoBlockWidth: false, // The blockWidth is dinamically calculated based on the autoBlockSize. Disabled blockWidth. Useful for responsive backgrounds
       autoBlockSize: 24, // The max size of a block for autowidth mode
       difficulty: 'normal', // Difficulty (normal|nice|evil).
@@ -442,7 +442,8 @@
               game._board.renderChanged = true;
             } else {
               this.orientation = orientation;
-              while (this.x >= 8){
+              
+              while ( this.x >= ( game._BLOCK_WIDTH - 2 )){
                 this.x--;
               } 
               while (this.x < 0){
